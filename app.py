a074c0dba065b69914329aea906b3acc984da218
rkumar061 <rkumarkumawat@protonmail.com>
2021-12-25 19:38:30 +0530

    mail1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from models import UserModel,db
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -7,12 +8,30 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_table.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['TESTING'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'truhelper6969@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Ucancallme@69'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = False
+app.config['MAIL_MAX_EMAILS'] = None
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+mail = Mail(app)
  
 @app.before_first_request
 def create_table():
     db.create_all()
 
-
+@app.route('/')
+def index():
+    msg = Message('Hello', recipients = ['magis96201@wwdee.com'] )
+    msg.body = 'Hello Flask message sent from Flask-Mail'
+    mail.send(msg)
+    return '<h1>Hello World</h1>'
 
 @app.route("/login", methods=['GET', 'POST'])
 def main():
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import requests
-res = requests.post('http://192.168.1.5:8000/login', json={"name":"name","email":"lulalula","password":"123"})
+res = requests.post('http://139.59.77.191:8000/login', json={"email":"email","password":"password@user"})
 if res.ok:
     print(res.json())
