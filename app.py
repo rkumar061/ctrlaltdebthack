947b9d4b58ac79e7ac376abf367a63bd8340b712
rkumar061 <rkumarkumawat@protonmail.com>
2021-12-26 14:26:56 +0530

    loginsignup

--- app.py
+++ app.py
@@ -25,18 +25,25 @@ def create():
  
     if request.method == 'POST':
         data = request.json
-        email = data['email']
-        password = data['password']
-        name = data['name']
-        role = data['role']
-        user = UserModel.query.filter_by(email=email).first()
-        if user:
-            return jsonify({"message": "fail"})
+        if data.get('name') and data.get('name') and data.get('password') and data.get('role'):
+            email = data['email']
+            password = data['password']
+            name = data['name']
+            role = data['role']
+            user = UserModel.query.filter_by(email=email).first()   
+            if user:
+                return jsonify({"message": "user already exists"})
+            else:
+                try:
+                    user = UserModel(name = name, email = email, password = password,role=role)
+                    db.session.add(user)
+                    db.session.commit()
+                    return jsonify({"email":email,"message": "success"})
+                except:
+                    return jsonify({"message": "something went wrong"})
+            
         else:
-            user = UserModel(name = name, email = email, password = password,role=role)
-            db.session.add(user)
-            db.session.commit()
-            return jsonify({"email":email,"created":True})
+            return jsonify({'message':'incomplete data'})
             
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -44,13 +51,16 @@ def main():
     
     if request.method == 'POST':
         data = request.json
-        email = data['email']
-        password = data['password']
-        user = UserModel.query.filter_by(email=email).first()
-        if user and user.password == password:
-            return jsonify({"message": "success", "name": user.name, "email": user.email, "role": user.role})
+        if data.get('email') and data.get('password'):
+            email = data['email']
+            password = data['password']
+            user = UserModel.query.filter_by(email=email).first()
+            if user and user.password == password:
+                return jsonify({"message": "success", "name": user.name, "email": user.email, "role": user.role})
+            else:
+                return jsonify({"message": "fail"})
         else:
-            return jsonify({"message": "fail"})
+            return jsonify({"message": "incomplete data"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
