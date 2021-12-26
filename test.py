e584f0dd1ca4d1c312347deeff717e01fdc3eccd
rkumar061 <rkumarkumawat@protonmail.com>
2021-12-26 09:44:37 +0530

    Revert "mail1"
    This reverts commit a074c0dba065b69914329aea906b3acc984da218.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import requests
-res = requests.post('http://139.59.77.191:8000/login', json={"email":"email","password":"password@user"})
+res = requests.post('http://192.168.1.5:8000/login', json={"name":"name","email":"lulalula","password":"123"})
 if res.ok:
     print(res.json())
